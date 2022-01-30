13cc4fc7d5e9f953e28f24b91f00f7f8639aa6f9
ashleyshi006 <yshi1325@conestogac.on.ca>
2022-01-30 16:15:39 -0500

    style and editor added

---
+++ Blog/Controllers/iFileManager.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.IO;
+
+namespace Blog.Controllers
+{
+    public class iFileManager
+    {
+        internal Stream ImageStream(string image) => throw new NotImplementedException();
+    }
+}
---
+++ Blog/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Post> Posts { get; set; }
+    }
+}
